fa7b6eb1c46b2af693e9e0f493249eef166786fb
Denis Melnikov <denmel02@gmail.com>
2025-09-29 18:52:59 +0300

    fix

--- src/client/refresh.ts
+++ src/client/refresh.ts
@@ -2,8 +2,10 @@ import { auth, tokensSchema } from 'features';
 import { refreshClient } from './refreshClient';
 import type { TTokens } from 'features';
 
+const REFRESH_URL = '/refresh';
+
 export const refresh = async () => {
-    const response = await refreshClient.post<TTokens>('/refresh', undefined, { schema: tokensSchema });
+    const response = await refreshClient.post<TTokens>(REFRESH_URL, undefined, { schema: tokensSchema });
 
     auth.saveTokens(response.data);
 
